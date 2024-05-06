dd3ae9113cdffb13ab87fc14344be3cd2f81e50a
Koji AGAWA <atty303@gmail.com>
2024-05-06 23:54:43 +0900

    wip

--- packages/driver/src/driver.c
+++ packages/driver/src/driver.c
@@ -1,7 +1,13 @@
+#include <stdio.h>
 #include <emscripten.h>
 #include "lua.h"
 
 lua_State *driver_new_state() {
   lua_State *L = lua_newstate(NULL, NULL);
   return L;
+}
+
+int init() {
+    printf("init\n");
+    return 2;
 }
--- packages/driver/src/main.ts
+++ packages/driver/src/main.ts
@@ -1,1 +1,10 @@
-console.log('driver');
+// @ts-ignore
+import {default as Module} from "../dist/driver.mjs";
+
+Module({
+    print: console.log,
+    printErr: console.error,
+}).then((module: any) => {
+    console.log(module);
+    console.log(module._init());
+});
