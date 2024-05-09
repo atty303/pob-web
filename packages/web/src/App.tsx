a4e27e25810819434e79727c12e54b948adb8629
Koji AGAWA <atty303@gmail.com>
2024-05-09 21:44:53 +0900

    chore: daisyui

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -3,7 +3,13 @@ import PobWindow from "./PobWindow.tsx";
 function App() {
   return (
       <>
-          <PobWindow/>
+          <div className="navbar bg-base-100">
+              <a className="btn btn-ghost text-xl">Path of Building Web</a>
+          </div>
+
+          <div className="p-4">
+              <PobWindow/>
+          </div>
       </>
   )
 }
