7780b13b48f77ee6bd3fcb11fc6734f1a3e67d02
Koji AGAWA <atty303@gmail.com>
2024-05-09 15:05:30 +0900

    build

--- packages/driver/vite.config.ts
+++ packages/driver/vite.config.ts
@@ -4,14 +4,13 @@ import * as path from "node:path";
 // // https://vitejs.dev/config/
 export default defineConfig({
     define: {
-        __DATA_PREFIX__: JSON.stringify("@fs/" + path.resolve("../driver/dist").replaceAll("\\", "/") + "/"),
+        __DATA_PREFIX__: JSON.stringify("@fs/" + path.resolve("emscripten").replaceAll("\\", "/") + "/"),
         __ASSET_PREFIX__: JSON.stringify("@fs/" + path.resolve("../../vendor/PathOfBuilding/src").replaceAll("\\", "/") + "/"),
     },
     server: {
         fs: {
             allow: [
                 ".",
-                "../driver/dist",
                 "../../vendor/PathOfBuilding/src",
             ],
         },
