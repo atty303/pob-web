af745627319e603b51296e5c029d1b575fd7e764
Koji AGAWA <atty303@gmail.com>
2024-05-10 06:33:22 +0900

    fix: driver shell

--- packages/driver/src/run.ts
+++ packages/driver/src/run.ts
@@ -4,5 +4,6 @@ const win = new PobWindow({
     container: document.querySelector("#window")!,
     dataPrefix: __DATA_PREFIX__,
     assetPrefix: __ASSET_PREFIX__,
+    onFrame: (_render, _time) => {},
 });
 win.start();
