bc7272774830d0e2d3941ec72df792fd5986ab91
Koji AGAWA <atty303@gmail.com>
2024-07-23 17:42:09 +0900

    feat(web): added version: v2.44.0

--- packages/web/src/Header.tsx
+++ packages/web/src/Header.tsx
@@ -91,6 +91,7 @@ function SettingDialog(props: {
               value={props.version}
               onChange={(e) => props.onVersionChange(e.target.value)}
             >
+              <option value="v2.44.0">2.44.0</option>
               <option value="v2.43.0">2.43.0</option>
               <option value="v2.42.0">2.42.0</option>
               <option value="v2.41.1">2.41.1</option>
