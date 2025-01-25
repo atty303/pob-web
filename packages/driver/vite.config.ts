ccc439e84e8c0d8df2ed8570db39f7d51365add6
Koji AGAWA <atty303@gmail.com>
2025-01-25 09:22:30 +0900

    chore: Update versioning logic and enhance Vite configuration
    Revised the version format in `run.ts` for better flexibility. Enhanced `vite.config.ts` to support workspace roots, adjusted asset prefix logic based on mode, and added the `vite-plugin-inspect` plugin for improved debugging.

--- packages/driver/vite.config.ts
+++ packages/driver/vite.config.ts
@@ -1,10 +1,21 @@
 import * as path from "node:path";
-import { defineConfig } from "vite";
+import { defineConfig, searchForWorkspaceRoot } from "vite";
+import Inspect from "vite-plugin-inspect";
+
+const packerBuildDir = path.resolve(__dirname, "../packer/build");
 
 // // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  logLevel: "info",
+  server: {
+    fs: {
+      allow: [searchForWorkspaceRoot(process.cwd()), packerBuildDir],
+    },
+  },
   define: {
-    __ASSET_PREFIX__: JSON.stringify("https://asset.pob.cool/versions"),
+    __ASSET_PREFIX__: JSON.stringify(
+      mode === "development" ? `/@fs/${packerBuildDir}` : "https://asset.pob.cool/versions",
+    ),
   },
   build: {
     sourcemap: true,
@@ -12,4 +23,5 @@ export default defineConfig({
   worker: {
     format: "es",
   },
-});
+  plugins: [Inspect()],
+}));
