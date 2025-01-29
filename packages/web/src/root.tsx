9829ffb7a6884cd9f6bdf0ab2908b00520dfaf92
Koji AGAWA <atty303@gmail.com>
2025-01-29 23:22:01 +0900

    fix(web): implement error boundary for route error handling

--- packages/web/src/root.tsx
+++ packages/web/src/root.tsx
@@ -5,7 +5,7 @@ import * as Sentry from "@sentry/react";
 import type React from "react";
 
 import "./lib/logger";
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration, isRouteErrorResponse } from "react-router";
 import { RecoilRoot } from "recoil";
 import type { Route } from "./+types/root";
 
@@ -55,7 +55,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body className="bg-base-content">
+      <body className="bg-base text-base-content ">
         {children}
         <ScrollRestoration />
         <Scripts />
@@ -85,28 +85,28 @@ export default function Root() {
   );
 }
 
-// export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-//   let message = "Oops!";
-//   let details = "An unexpected error occurred.";
-//   let stack: string | undefined;
-//
-//   if (isRouteErrorResponse(error)) {
-//     message = error.status === 404 ? "404" : "Error";
-//     details = error.status === 404 ? "The requested page could not be found." : error.statusText || details;
-//   } else if (import.meta.env.DEV && error && error instanceof Error) {
-//     details = error.message;
-//     stack = error.stack;
-//   }
-//
-//   return (
-//     <main className="pt-16 p-4 container mx-auto">
-//       <h1>{message}</h1>
-//       <p>{details}</p>
-//       {stack && (
-//         <pre className="w-full p-4 overflow-x-auto">
-//           <code>{stack}</code>
-//         </pre>
-//       )}
-//     </main>
-//   );
-// }
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+  let message = "Oops!";
+  let details = "An unexpected error occurred.";
+  let stack: string | undefined;
+
+  if (isRouteErrorResponse(error)) {
+    message = error.status === 404 ? "404" : "Error";
+    details = error.status === 404 ? "The requested page could not be found." : error.statusText || details;
+  } else if (import.meta.env.DEV && error && error instanceof Error) {
+    details = error.message;
+    stack = error.stack;
+  }
+
+  return (
+    <main className="p-16 max-w-full prose">
+      <h1>{message}</h1>
+      <p>{details}</p>
+      {stack && (
+        <pre className="w-full p-4 overflow-x-auto">
+          <code>{stack}</code>
+        </pre>
+      )}
+    </main>
+  );
+}
