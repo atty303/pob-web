6a5331d57a97fd07bd54130d07e5c16c0512d618
Koji AGAWA <atty303@gmail.com>
2025-02-23 19:12:25 +0900

    fix(packer): sync v1 path

--- packages/packer/src/sync.ts
+++ packages/packer/src/sync.ts
@@ -15,7 +15,8 @@ if (!product) {
   process.exit(1);
 }
 
+const dst = product === 1 ? `s3://pob-web/versions/${tag}` : `s3://pob-web/versions.${product}/${tag}`;
 shelljs.exec(
-  `aws s3 sync --region auto --endpoint-url ${process.env.R2_ENDPOINT_URL} build/${product}/${tag}/r2 s3://pob-web/versions.${product}/${tag}`,
+  `aws s3 sync --checksum-algorithm CRC32 --region auto --endpoint-url ${process.env.R2_ENDPOINT_URL} build/${product}/${tag}/r2 ${dst}`,
   { fatal: true },
 );
