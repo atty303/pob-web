2d16e3a237728eba08df1a9c7e38a2e7301903db
Koji AGAWA <atty303@gmail.com>
2025-04-28 02:47:29 +0000

    fix(dds.test.ts): update file path for reading DDS data

--- packages/dds/src/dds.test.ts
+++ packages/dds/src/dds.test.ts
@@ -6,9 +6,9 @@ await zstd.init();
 
 const file = fs.readFileSync(
   // "../packer/build/2/v0.3.0/repo/src/TreeData/0_1/ascendancy-background_1500_1500_BC7.dds.zst",
-  "../packer/build/2/v0.3.0/repo/src/TreeData/0_1/skills-disabled_128_128_BC1.dds.zst",
+  // "../packer/build/2/v0.6.0/repo/src/TreeData/0_1/skills_176_176_BC1.dds.zst",
+  "../packer/build/2/v0.6.0/repo/src/TreeData/0_2/skills_176_176_BC1.dds.zst",
 );
 const data = zstd.decompress(new Uint8Array(file));
 fs.writeFileSync("dds.dds", data);
 const dds = parseDDSDX10(data);
-console.log(dds);
