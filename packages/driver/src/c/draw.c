60ecb3ce719ffb5d424c660a6fe952922960aa6f
Koji AGAWA <atty303@gmail.com>
2025-08-14 16:34:10 +0900

    perf(driver): enhance data structure alignment and command handling in draw module

--- packages/driver/src/c/draw.c
+++ packages/driver/src/c/draw.c
@@ -20,32 +20,30 @@ typedef enum {
     DRAW_STRING = 8,
 } DrawCommandType;
 
-#pragma pack(push, 1)
-
 typedef struct {
-    uint8_t type;
-    int16_t layer;
-    int16_t sublayer;
+    int type;
+    int layer;
+    int sublayer;
 } SetLayerCommand;
 
 typedef struct {
-    uint8_t type;
+    int type;
     int x, y, w, h;
 } SetViewportCommand;
 
 typedef struct {
-    uint8_t type;
-    uint8_t r, g, b, a;
+    int type;
+    int r, g, b, a;
 } SetColorCommand;
 
 typedef struct {
-    uint8_t type;
-    uint16_t text_size;
+    int type;
+    int text_size;
     char text[];
 } SetColorEscapeCommand;
 
 typedef struct {
-    uint8_t type;
+    int type;
     int image_handle;
     float x, y, w, h;
     float s1, t1, s2, t2;
@@ -54,7 +52,7 @@ typedef struct {
 } DrawImageCommand;
 
 typedef struct {
-    uint8_t type;
+    int type;
     int image_handle;
     float x1, y1, x2, y2, x3, y3, x4, y4;
     float s1, t1, s2, t2, s3, t3, s4, t4;
@@ -63,16 +61,14 @@ typedef struct {
 } DrawImageQuadCommand;
 
 typedef struct {
-    uint8_t type;
+    int type;
     float x, y;
-    uint8_t align;
-    uint8_t height;
-    uint8_t font;
-    uint16_t text_size;
+    int align;
+    int height;
+    int font;
+    int text_size;
     char text[];
 } DrawStringCommand;
-
-#pragma pack(pop)
 
 typedef struct {
     uint8_t *data;
